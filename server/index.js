53286b1106cd50cb4f434613ef094fc2a8b0a7a7
Abdessamad7687 <abdessamadahmali@gmail.com>
2023-04-14 20:53:54 +0000

    V1.0

--- server/index.js
+++ server/index.js
@@ -1,12 +1,8 @@
-// import express from "express"
-// import mongoose from "mongoose"
-// import dotenv from "dotenv"
-// import {router} from "./routes/BooksRoutes.js"
 
 const express = require('express')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
-const router = require('./routes/BooksRoutes.js')
+const router = require('./routes/BooksRoutes')
 
 
 const app = express()
@@ -17,6 +13,4 @@ app.listen(process.env.PORT, () => console.log('app running on port ', process.e
 mongoose.connect(process.env.CONNECTION)
 .then(() => console.log('database connected'))
 
-app.use('/', router)
-
-
+// app.use('/', router)
--- server/routes/BooksRoutes.js
+++ server/routes/BooksRoutes.js
@@ -1,7 +1,5 @@
 const express = require('express')
-
 const router = express.Router()
-
 const BookController = require('../controllers/BookController')
 
 router.get('/', BookController.getBooks)
